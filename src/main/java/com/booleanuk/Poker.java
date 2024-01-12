9cccf47aebbc428a463a3564fe7b8bb5553d0ada
Espen Solhaug <espen.solhaug@no.experis.com>
2024-01-12 14:39:03 +0100

    Finished Exercise, Extention1. One fail on Extention2.

--- src/main/java/com/booleanuk/Poker.java
+++ src/main/java/com/booleanuk/Poker.java
@@ -1,25 +1,160 @@
 package com.booleanuk;
 
+import java.util.*;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.Integer.valueOf;
+import static java.util.Collections.max;
+
 public class Poker {
+
+    HashMap<String, Integer> cards = new HashMap<String, Integer>() {{
+        put("1", 1);
+        put("2", 2);
+        put("3", 3);
+        put("4", 4);
+        put("5", 5);
+        put("6", 6);
+        put("7", 7);
+        put("8", 8);
+        put("9", 9);
+        put("10", 10);
+        put("J", 11);
+        put("Q", 12);
+        put("K", 13);
+        put("A", 14);
+    }};
     // Core
     public String[] winningPair(String[] firstHand, String[] secondHand) {
         // Implement the winningPair logic here and return the array containing the winning pair to make the tests pass.
         // You can replace the following return value with something appropriate
-        return new String[]{"Replace me", "with something else"};
+        int highestPairFirstHand = getHighestPairFromHand(firstHand);
+        int highestPairSecondHand = getHighestPairFromHand(secondHand);
+
+        if(highestPairFirstHand > highestPairSecondHand) {
+            return firstHand;
+        } else if(highestPairFirstHand < highestPairSecondHand) {
+            return secondHand;
+        } else {
+            return new String[] {};
+        }
+    }
+
+    public static void main(String[] args) {
+        Poker poker = new Poker();
+        poker.winningPair(new String[] {"3", "6", "5", "7", "9", "7"}, new String[] {"2", "5"} );
+    }
+
+    public int getHighestPairFromHand(String[] hand) {
+        HashMap<String, Integer> cardCountMap = new HashMap<>();
+        for(String card : hand) {
+            cardCountMap.put(card, cardCountMap.getOrDefault(card, 0) + 1);
+        }
+        int highestPair = -1;
+        for(Map.Entry<String, Integer> entry : cardCountMap.entrySet()) {
+            String card = entry.getKey();
+            int value = entry.getValue();
+
+            if(value == 2) {
+                int cardValue = cards.get(card);
+
+                if (cardValue > highestPair) {
+                    highestPair = cardValue;
+                }
+            }
+        }
+        return highestPair;
     }
 
     // Extension 1
     public String[] winningPairFromArray(String[][] hands) {
-        // Implement the winningPairFromArray logic here and return the array containing the winning pair to make the tests pass.
-        // You can replace the following return value with something appropriate
-        return new String[]{"Replace me", "with something else"};
+        int[] handArray = new int[hands.length];
+        for(int i = 0; i < hands.length; i++) {
+            handArray[i] = getHighestPairFromHand(hands[i]);
+        }
+        int maxAt = 0;
+        for (int i = 0; i < handArray.length; i++) {
+            if(handArray[maxAt] < handArray[i]) {
+                maxAt = i;
+            }
+        }
+        if(maxAt != 0) {
+            return hands[maxAt];
+        } else {
+            return new String[]{};
+        }
     }
 
     // Extension 2
     public String[] winningThreeCardHand(String[][] hands) {
-        // Implement the winningThreeCardHand logic here and return the array containing the winning hand to make the tests pass.
-        // You can replace the following return value with something appropriate
-        return new String[]{"Replace me", "with something else"};
+        int[] handArray = new int[hands.length];
+        for(int i = 0; i < hands.length; i++) {
+            handArray[i] = getHighestThreeFromHand(hands[i]);
+        }
+        int maxAt = 0;
+        for (int i = 0; i < handArray.length; i++) {
+            if(handArray[maxAt] < handArray[i]) {
+                maxAt = i;
+            }
+        }
+        if(maxAt != 0) {
+            return hands[maxAt];
+        } else {
+            String[] highestPair = new String[] {"0","0"};
+            for(String[] hand : hands) {
+                if(getHighestPairThreeFromHand(hand) > parseInt((highestPair[0]))) {
+                    highestPair = hand;
+                }
+            }
+            if(highestPair[0].equals("0")) {
+                return new String[]{};
+            } else {
+                return highestPair;
+            }
+
+        }
+    }
+
+    public int getHighestThreeFromHand(String[] hand) {
+        HashMap<String, Integer> cardCountMap = new HashMap<>();
+        for(String card : hand) {
+            cardCountMap.put(card, cardCountMap.getOrDefault(card, 0) + 1);
+        }
+        int highestPair = -1;
+        for(Map.Entry<String, Integer> entry : cardCountMap.entrySet()) {
+            String card = entry.getKey();
+            int value = entry.getValue();
+
+            if(value == 3) {
+                int cardValue = cards.get(card);
+
+                if (cardValue > highestPair) {
+                    highestPair = cardValue;
+                }
+            }
+        }
+        return highestPair;
+    }
+
+    public int getHighestPairThreeFromHand(String[] hand) {
+        HashMap<String, Integer> cardCountMap = new HashMap<>();
+        for(String card : hand) {
+            cardCountMap.put(card, cardCountMap.getOrDefault(card, 0) + 1);
+        }
+        int highestPair = -1;
+        for(Map.Entry<String, Integer> entry : cardCountMap.entrySet()) {
+            String card = entry.getKey();
+            int value = entry.getValue();
+
+            if(value == 2) {
+                int cardValue = cards.get(card);
+
+                if (cardValue > highestPair) {
+                    highestPair = cardValue;
+                }
+            }
+        }
+        return highestPair;
     }
 
     // Extension 3
