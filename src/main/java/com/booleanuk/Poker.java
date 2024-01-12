e5b4331bc8b91a3b67ad814de8e4bfa320959b91
ThomasKvam <thomas.e.h.kvam@hotmail.no>
2024-01-12 15:20:27 +0100

    Done with core.

--- src/main/java/com/booleanuk/Poker.java
+++ src/main/java/com/booleanuk/Poker.java
@@ -1,11 +1,96 @@
 package com.booleanuk;
 
+import java.util.HashMap;
+
 public class Poker {
     // Core
+    HashMap<String,Integer> cardValue = new HashMap<>();
+    boolean pair;
+    boolean threeOfAKind;
+    boolean fourOfAKind;
+    boolean fullHouse;
+
+
+    public Poker(){
+        cardValue.put("2",2);
+        cardValue.put("3",3);
+        cardValue.put("4",4);
+        cardValue.put("5",5);
+        cardValue.put("6",6);
+        cardValue.put("7",7);
+        cardValue.put("8",8);
+        cardValue.put("9",9);
+        cardValue.put("10",10);
+        cardValue.put("J",11);
+        cardValue.put("Q",12);
+        cardValue.put("K",13);
+        cardValue.put("A",14);
+
+        pair = false;
+        threeOfAKind = false;
+        fourOfAKind = false;
+        fullHouse = false;
+    }
+
+    public boolean isPair(String[] hand){
+        int i = 0;
+        if (hand[i].equals(hand[i + 1])){
+            return true;
+        }
+        return false;
+    }
+
+
     public String[] winningPair(String[] firstHand, String[] secondHand) {
         // Implement the winningPair logic here and return the array containing the winning pair to make the tests pass.
         // You can replace the following return value with something appropriate
-        return new String[]{"Replace me", "with something else"};
+        int totalvalue1 = 0;
+        int totalvalue2 = 0;
+        int index = 0;
+        String[] noPair = new String[0];
+
+        if (isPair(firstHand) && isPair(secondHand)) {
+            for (String string : firstHand) {
+                totalvalue1 += cardValue.get(string);
+            }
+            for (String string : secondHand) {
+                totalvalue2 += cardValue.get(string);
+            }
+            if (totalvalue1 > totalvalue2){
+                return firstHand;
+            }
+            else{
+                return secondHand;
+            }
+        }
+
+        else if (isPair(firstHand) && !isPair(secondHand)) {
+            return firstHand;
+        }
+
+        else if (isPair(secondHand) && !isPair(firstHand)) {
+            return secondHand;
+        }
+
+            return noPair;
+
+        /*
+        else if (!isPair(firstHand) && !isPair(secondHand)){
+            for (String string : firstHand) {
+                totalvalue1 += cardValue.get(string);
+            }
+            for (String string : secondHand) {
+                totalvalue2 += cardValue.get(string);
+            }
+        }
+        if (totalvalue1 > totalvalue2){
+            return firstHand;
+        }
+        else{
+            return secondHand;
+        }
+
+         */
     }
 
     // Extension 1
