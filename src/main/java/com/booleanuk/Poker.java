268c30b197c51ff05c0d8817665210b5c9f42f36
pkekkonen <paulina.kekkonen@hotmail.com>
2024-01-12 13:03:02 +0100

    Refactor

--- src/main/java/com/booleanuk/Poker.java
+++ src/main/java/com/booleanuk/Poker.java
@@ -3,38 +3,7 @@ package com.booleanuk;
 public class Poker {
     // Core
     public String[] winningPair(String[] firstHand, String[] secondHand) {
-        if(firstHand[0].equals(firstHand[1])) {
-            if(secondHand[0].equals(secondHand[1])) {
-                int firstHandValue = getCardValue(firstHand[0]);
-                int secondHandValue = getCardValue(secondHand[0]);
-
-                if(firstHandValue > secondHandValue) {
-                    return firstHand;
-                } else if(firstHandValue < secondHandValue) {
-                    return secondHand;
-                } else {
-                    return new String[]{};
-                }
-            } else {
-                return firstHand;
-            }
-        } else if(secondHand[0].equals(secondHand[1])) {
-            return secondHand;
-        } else {
-            return new String[]{};
-        }
-    }
-
-    private int getCardValue(String card) {
-        int value;
-        switch(card.toLowerCase()) {
-            case "j" -> value = 11;
-            case "q" -> value = 12;
-            case "k" -> value = 13;
-            case "a" -> value = 14;
-            default -> value = Integer.parseInt(card);
-        }
-        return value;
+        return winningPairFromArray(new String[][]{firstHand, secondHand});
     }
 
     // Extension 1
@@ -58,38 +27,38 @@ public class Poker {
     // Extension 2
     public String[] winningThreeCardHand(String[][] hands) {
         int highestPairValue = 0, highestThreeOfKindValue = 0;
-        int currentValue;
+        int currentValue = -1;
+        String currentKind;
         String[] highestPairHand = new String[]{};
         String[] highestThreeOfKindHand = new String[]{};
 
         for(String[] hand: hands) {
-            if(hand.length == 2 && highestThreeOfKindValue == 0) {
-                if (hand[0].equals(hand[1])) {
-                    currentValue = getCardValue(hand[0]);
-                    if (currentValue > highestPairValue) {
-                        highestPairValue = currentValue;
-                        highestPairHand = hand;
-                    }
-                }
+            currentKind = "";
+            if(hand.length == 2 && highestThreeOfKindValue == 0 && hand[0].equals(hand[1])) {
+                currentValue = getCardValue(hand[0]);
+                currentKind = "pair";
             } else if(hand.length == 3){
                 if(hand[0].equals(hand[1]) && hand[0].equals(hand[2])) {
                     currentValue = getCardValue(hand[0]);
-                    if (currentValue > highestThreeOfKindValue) {
-                        highestThreeOfKindValue = currentValue;
-                        highestThreeOfKindHand = hand;
-                    }
+                    currentKind = "three of kind";
                 } else if(hand[0].equals(hand[1]) || hand[0].equals(hand[2])) {
                     currentValue = getCardValue(hand[0]);
-                    if (currentValue > highestPairValue) {
-                        highestPairValue = currentValue;
-                        highestPairHand = hand;
-                    }
+                    currentKind = "pair";
                 } else if(hand[1].equals(hand[2])) {
                     currentValue = getCardValue(hand[1]);
-                    if (currentValue > highestPairValue) {
-                        highestPairValue = currentValue;
-                        highestPairHand = hand;
-                    }
+                    currentKind = "pair";
+                }
+            }
+
+            if(currentKind.equals("pair")) {
+                if (currentValue > highestPairValue) {
+                    highestPairValue = currentValue;
+                    highestPairHand = hand;
+                }
+            } else if(currentKind.equals("three of kind")) {
+                if (currentValue > highestThreeOfKindValue) {
+                    highestThreeOfKindValue = currentValue;
+                    highestThreeOfKindHand = hand;
                 }
             }
         }
@@ -102,5 +71,17 @@ public class Poker {
         // Completely replace this method with suitable methods to solve Extension 3
         // You will also need to add the relevant tests to Extension3Test.java
         return "Replace this method with your own methods and tests";
+    }
+
+    private int getCardValue(String card) {
+        int value;
+        switch(card.toLowerCase()) {
+            case "j" -> value = 11;
+            case "q" -> value = 12;
+            case "k" -> value = 13;
+            case "a" -> value = 14;
+            default -> value = Integer.parseInt(card);
+        }
+        return value;
     }
 }
