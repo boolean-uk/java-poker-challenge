759f34128e5b43f088c302d2f9219712408a0184
hanna <hannaphamm1007@gmail.com>
2024-01-12 23:37:58 +0100

    completed extension1

--- src/main/java/com/booleanuk/Poker.java
+++ src/main/java/com/booleanuk/Poker.java
@@ -53,7 +53,21 @@ public class Poker {
     public String[] winningPairFromArray(String[][] hands) {
         // Implement the winningPairFromArray logic here and return the array containing the winning pair to make the tests pass.
         // You can replace the following return value with something appropriate
-        return new String[]{"Replace me", "with something else"};
+        String[] winPair = new String[2];
+        int maxValue = 0;
+        for(int i = 0; i<hands.length; i++){
+            if(hands[i][0].equals(hands[i][1])){
+                int currentValue = theCard.get(hands[i][0]);
+                if(currentValue > maxValue){
+                    maxValue = currentValue;
+                    winPair = hands[i];
+                }
+            }
+        }
+        if(maxValue == 0){
+            return new String[]{};
+        }
+        return winPair;
     }
 
     // Extension 2
