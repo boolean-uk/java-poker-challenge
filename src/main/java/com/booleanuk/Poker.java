638962c83885f65158489a3acd1b6b65e5cfab4e
pkekkonen <paulina.kekkonen@hotmail.com>
2024-01-12 12:20:12 +0100

    Core and first extension completed

--- src/main/java/com/booleanuk/Poker.java
+++ src/main/java/com/booleanuk/Poker.java
@@ -5,14 +5,58 @@ public class Poker {
     public String[] winningPair(String[] firstHand, String[] secondHand) {
         // Implement the winningPair logic here and return the array containing the winning pair to make the tests pass.
         // You can replace the following return value with something appropriate
-        return new String[]{"Replace me", "with something else"};
+        if(firstHand[0].equals(firstHand[1])) {
+            if(secondHand[0].equals(secondHand[1])) {
+                int firstHandValue = getCardValue(firstHand[0]);
+                int secondHandValue = getCardValue(secondHand[0]);
+
+                if(firstHandValue > secondHandValue) {
+                    return firstHand;
+                } else if(firstHandValue < secondHandValue) {
+                    return secondHand;
+                } else {
+                    return new String[]{};
+                }
+            } else {
+                return firstHand;
+            }
+        } else if(secondHand[0].equals(secondHand[1])) {
+            return secondHand;
+        } else {
+            return new String[]{};
+        }
+    }
+
+    private int getCardValue(String card) {
+        int value;
+        switch(card.toLowerCase()) {
+            case "j" -> value = 11;
+            case "q" -> value = 12;
+            case "k" -> value = 13;
+            case "a" -> value = 14;
+            default -> value = Integer.parseInt(card);
+        }
+        return value;
     }
 
     // Extension 1
     public String[] winningPairFromArray(String[][] hands) {
         // Implement the winningPairFromArray logic here and return the array containing the winning pair to make the tests pass.
         // You can replace the following return value with something appropriate
-        return new String[]{"Replace me", "with something else"};
+        int highestValue = 0, currentValue;
+        String[] highestHand = new String[]{};
+
+        for(String[] hand: hands) {
+            if(hand[0].equals(hand[1])) {
+                currentValue = getCardValue(hand[0]);
+                if(currentValue > highestValue) {
+                    highestValue = currentValue;
+                    highestHand = hand;
+                }
+            }
+        }
+
+        return highestHand;
     }
 
     // Extension 2
