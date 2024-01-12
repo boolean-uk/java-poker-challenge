ca47d28b4cedc8fc768f24dbefe4ba1e7012dd8b
Vegard Vognstolen <vegard.bruas.vognstolen@no.experis.com>
2024-01-12 16:07:09 +0100

    Vegard Bruas Vognstolen

--- src/test/java/com/booleanuk/extension/Extension3Test.java
+++ src/test/java/com/booleanuk/extension/Extension3Test.java
@@ -4,9 +4,39 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class Extension3Test {
+
+
     @Test
-    public void shouldPass() {
+    public void royalFlush() {
         Poker poker = new Poker();
-        Assertions.assertEquals("Replace this method with your own methods and tests", poker.extensionThreeMethods());
+        String[][] hands = {new String[]{"A", "K", "Q", "J", "10"}, new String[]{"9", "9","9", "9", "2"}, new String[]{"6", "6", "4"}};
+        String[] result = poker.extensionThreeMethods(hands);
+        Assertions.assertArrayEquals(new String[]{"A", "K", "Q", "J", "10"}, result);
     }
+
+    @Test
+    public void testSorting() {
+        Poker poker = new Poker();
+        String[] hand = new String[]{"A", "K", "Q", "J", "10","3","2","5"};
+        String[] result = poker.sortStringArrayOfCards(hand);
+        Assertions.assertArrayEquals(new String[]{"2", "3", "5", "10", "J", "Q", "K", "A"}, result);
+    }
+
+
+    @Test
+    public void testTwoThreeFourOfAKind() {
+        Poker poker = new Poker();
+        String[] hand = new String[] {"A","A","A","K","K"};
+        int[] result = poker.howManyTwoThreeFourOfAKind(hand);
+        Assertions.assertArrayEquals(new int[]{1,1,0}, result);
+    }
+
+    @Test
+    public void testIsItFlush() {
+        Poker poker = new Poker();
+        String[] hand = new String[] {"3","2","5","6","4"};
+        boolean result= poker.isItStraight(hand);
+        Assertions.assertTrue(result);
+    }
+
 }
