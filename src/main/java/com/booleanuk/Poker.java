68b750d5fb4a4af4190a7bdb697d8ce26c6beb24
Hiko Yüksek <33344671+hkyksk@users.noreply.github.com>
2024-01-15 13:55:11 +0100

    Hikmet Yuksek

--- src/main/java/com/booleanuk/Poker.java
+++ src/main/java/com/booleanuk/Poker.java
@@ -2,6 +2,9 @@ package com.booleanuk;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Arrays;
+import java.util.List;
+
 
 
 public class Poker {
@@ -18,7 +21,12 @@ public class Poker {
     }
 
     public boolean pair(String[] hand) {
-        return hand[0].equalsIgnoreCase(hand[1]);
+        if (hand.length == 2 || hand.length == 3) {
+            return hand[0].equalsIgnoreCase(hand[1]);
+        } else if (hand.length == 3){
+            return (hand[1].equalsIgnoreCase(hand[2]) ||  hand[1].equalsIgnoreCase(hand[3]));
+        }
+        return false;
     }
 
     public String[] winningPair(String[] firstHand, String[] secondHand) {
@@ -45,26 +53,24 @@ public class Poker {
         // You can replace the following return value with something appropriate
         String[] winningPair = new String[]{};
         ArrayList<Integer> score = new ArrayList<>();
-        int highScore = 0;
-        int indexScore = 0;
 
         for (String[] hand : hands) {
             if (pair(hand)) {
-                score.add(pokerValues.get(hand[0]) * 10);
+                score.add(pokerValues.get(hand[0]));
             } else if (!pair(hand)) {
-                int score1 = pokerValues.get(hand[0]);
-                int score2 = pokerValues.get(hand[1]);
-                score.add(Math.max(score1, score2));
+                score.add(0);
             }
         }
 
-        for (int i = 1; i < score.size(); i++) {
-            if (score.get(i) > highScore) {
+        int highScore = score.get(0);
+        int index = 0;
+
+        for (int i = 0; i < score.size(); i++) {
+            if (highScore < score.get(i)) {
                 highScore = score.get(i);
-                indexScore = i;
+                winningPair = hands[i];
             }
         }
-        winningPair = hands[indexScore];
         return winningPair;
     }
 
@@ -72,10 +78,24 @@ public class Poker {
     public String[] winningThreeCardHand(String[][] hands) {
         // Implement the winningThreeCardHand logic here and return the array containing the winning hand to make the tests pass.
         // You can replace the following return value with something appropriate
-        return new String[]{"Replace me", "with something else"};
-    }
+        String[] winningHand = new String[]{};
+        int highScore = 0;
 
-    // Extension 3
+        for (String[] hand : hands) {
+            if (pair(hand)) {
+                int currentScore = pokerValues.get(hand[0]);
+                if (hand.length == 3) {
+                    currentScore += pokerValues.get(hand[2])*3;
+                }
+                if (currentScore > highScore) {
+                    highScore = currentScore;
+                    winningHand = hand;
+                }
+            }
+        }
+        return winningHand;
+    }
+        // Extension 3
     public String extensionThreeMethods() {
         // Completely replace this method with suitable methods to solve Extension 3
         // You will also need to add the relevant tests to Extension3Test.java
