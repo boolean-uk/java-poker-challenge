5ad9d42d2876ea673b55b2a69109d4f9f13e787f
Hassan Hussain <hassanmehmodhussain@gmail.com>
2024-02-25 20:53:11 +0100

    core extension finished

--- src/main/java/com/booleanuk/Poker.java
+++ src/main/java/com/booleanuk/Poker.java
@@ -58,35 +58,79 @@ public class Poker {
         map.put("K", 13);
         map.put("A", 14);
 
-        int[] firstPair = {map.get(hands[0][0]), map.get(hands[0][1])};
-        int[] secondPair = {map.get(hands[1][0]), map.get(hands[1][1])};
 
-        Arrays.sort(firstPair);
-        Arrays.sort(secondPair);
+            String[] winner = new String[0];
+            int highestPairValue = 0;
 
-        String[] pair1 = {hands[0][0], hands[0][1]};
-        String[] pair2 = {hands[1][0], hands[1][1]};
+            for (String[] hand : hands) {
+                int[] pair = {map.get(hand[0]), map.get(hand[1])};
+                Arrays.sort(pair);
 
-        if (firstPair[0] == firstPair[1] && secondPair[0] == secondPair[1]) { //are both pairs???
-            return (map.get(pair1[0]) > map.get(pair2[0])) ? pair1 : pair2; //ternary statement, that checks what is greater between the two values and returns it
-        } else if (firstPair[0] == firstPair[1]) { //does firsthand have pairs?
-            return pair1;
-        } else if (secondPair[0] == secondPair[1]) {//does secondhand have pairs?
-            return pair2;
-        }
+                if (pair[0] == pair[1] && pair[1] > highestPairValue) {
+                    highestPairValue = pair[1];
+                    winner = hand;
+                }
+            }
 
-        return new String[0];
-
-        // Implement the winningPairFromArray logic here and return the array containing the winning pair to make the tests pass.
-        // You can replace the following return value with something appropriate
+            return winner;
 
     }
 
+
+
+
     // Extension 2
     public String[] winningThreeCardHand(String[][] hands) {
-        // Implement the winningThreeCardHand logic here and return the array containing the winning hand to make the tests pass.
-        // You can replace the following return value with something appropriate
-        return new String[]{"Replace me", "with something else"};
+
+            HashMap<String, Integer> map = new HashMap<>();
+            map.put("2", 2);
+            map.put("3", 3);
+            map.put("4", 4);
+            map.put("5", 5);
+            map.put("6", 6);
+            map.put("7", 7);
+            map.put("8", 8);
+            map.put("9", 9);
+            map.put("10", 10);
+            map.put("J", 11);
+            map.put("Q", 12);
+            map.put("K", 13);
+            map.put("A", 14);
+
+            String[] winner = new String[0];
+            int highestPairValue = 0;
+            int highestThreeOfAKindValue = 0;
+
+            for (String[] hand : hands) {
+                int[] cardValues = new int[hand.length];
+                for (int i = 0; i < hand.length; i++) {
+                    cardValues[i] = map.get(hand[i]);
+                }
+
+                Arrays.sort(cardValues);
+
+                if (hand.length == 2) {
+                    if (cardValues[0] == cardValues[1] && cardValues[1] > highestPairValue) {
+                        highestPairValue = cardValues[1];
+                        winner = hand;
+                    }
+                } else if (hand.length == 3) {
+                    if (cardValues[0] == cardValues[1] && cardValues[1] == cardValues[2] && cardValues[2] > highestThreeOfAKindValue) {
+                        highestThreeOfAKindValue = cardValues[2];
+                        winner = hand;
+                    }
+                    else if (cardValues[0] == cardValues[1] && cardValues[1] > highestPairValue) {
+                        highestPairValue = cardValues[1];
+                        winner = hand;
+                    } else if (cardValues[1] == cardValues[2] && cardValues[2] > highestPairValue) {
+                        highestPairValue = cardValues[2];
+                        winner = hand;
+                    }
+                }
+            }
+
+            return winner;
+
     }
 
     // Extension 3
